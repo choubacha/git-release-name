cf1b129301d518eb3acb720bef5fbd417e1ab8d5
Kevin Choubacha <kchoubacha@weedmaps.com>
2018-01-08 15:11:40 -0800

    Read first arg as sha

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+use std::env;
 
 #[derive(Debug)]
 struct Word {
@@ -11,7 +12,7 @@ impl Word {
     fn new(value: &str) -> Word {
         Word {
             value: value.to_string(),
-            hash: usize::from_str_radix(&value, 16).expect("Value cannot be translated to a word index"),
+            hash: usize::from_str_radix(&value, 16).expect("Sha is not valid hex"),
             word: None,
         }
     }
@@ -26,15 +27,17 @@ mod nouns;
 mod adjectives;
 
 fn main() {
-    let sha = format!("{:8x}", rand::random::<usize>());
+    let args: Vec<String> = env::args().collect();
+    let sha = if args.len() > 1 {
+        format!("{:0>8}", args[1])
+    } else {
+        format!("{:8x}", rand::random::<usize>())
+    };
 
     let adv = Word::new(&sha[0..3]).use_dictionary(&adverbs::WORDS);
     let adj = Word::new(&sha[3..5]).use_dictionary(&adjectives::WORDS);
     let n   = Word::new(&sha[5..8]).use_dictionary(&nouns::WORDS);
 
-    println!("adv: {:?}", adv);
-    println!("adj: {:?}", adj);
-    println!("n:   {:?}", n);
     println!("{} {} {}",
              adv.word.unwrap(),
              adj.word.unwrap(),
