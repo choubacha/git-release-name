aaf7b7d6884b7732d23cb06bbd506e77d5f20bb1
Kevin Choubacha <kchoubacha@weedmaps.com>
2018-01-11 07:46:00 -0800

    Implement FromStr for Word and Phrase
    To simplify the parsing, I've refactored much of Word and Phrase. This
    makes some of the implementations much easier to manage. I've also taken
    `lookup` out of word to make it a pure function.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ extern crate clap;
 use std::fmt::{Display, Formatter, Error};
 use std::io::{self, BufRead};
 use atty::Stream;
-use clap::{Arg, App};
+use clap::{Arg, App, ArgMatches};
+use std::str::FromStr;
+
+mod adverbs;
+mod nouns;
+mod adjectives;
+
+#[derive(Debug, Eq, PartialEq)]
+enum PhraseParseError {
+    NonHexadecimalCharacters,
+}
+
+type ShaResult<T> = Result<T, PhraseParseError>;
 
 #[derive(Debug)]
 struct Word {
@@ -14,17 +26,26 @@ struct Word {
     word: Option<String>,
 }
 
-impl Word {
-    fn new(sha: &str) -> Word {
-        Word {
-            sha: sha.to_string(),
-            hash: usize::from_str_radix(&sha, 16).expect("Sha is not valid hex"),
-            word: None,
-        }
+fn lookup(word: Word, dict: &[&str]) -> Word {
+    Word {
+        word: dict.get(word.hash).map(|s| s.to_string()),
+        ..word
     }
+}
 
-    fn lookup(self, dict: &[&str]) -> Word {
-        Word { word: dict.get(self.hash).map(|s| s.to_string()), ..self }
+impl FromStr for Word {
+    type Err = PhraseParseError;
+
+    fn from_str(sha: &str) -> ShaResult<Word> {
+        if let Ok(hash) = usize::from_str_radix(&sha, 16) {
+            Ok(Word {
+                sha: sha.to_string(),
+                hash,
+                word: None,
+            })
+        } else {
+            Err(PhraseParseError::NonHexadecimalCharacters)
+        }
     }
 }
 
@@ -37,26 +58,24 @@ impl Display for Word {
     }
 }
 
-mod adverbs;
-mod nouns;
-mod adjectives;
-
 struct Phrase {
     adj: Word,
     adv: Word,
     noun: Word,
 }
 
-impl Phrase {
-    fn new(sha: &str) -> Phrase {
+impl FromStr for Phrase {
+    type Err = PhraseParseError;
+
+    fn from_str(sha: &str) -> ShaResult<Phrase> {
         // Ensure that the sha is at least 8 characters so that
         // when we extract the first 8 there is something there.
         let sha = format!("{:0>8}", sha);
-        let adv  = Word::new(&sha[0..3]).lookup(&adverbs::WORDS);
-        let adj  = Word::new(&sha[3..5]).lookup(&adjectives::WORDS);
-        let noun = Word::new(&sha[5..8]).lookup(&nouns::WORDS);
+        let adv = lookup(sha[0..3].parse()?, &adverbs::WORDS);
+        let adj = lookup(sha[3..5].parse()?, &adjectives::WORDS);
+        let noun = lookup(sha[5..8].parse()?, &nouns::WORDS);
 
-        Phrase { adv, adj, noun }
+        Ok(Phrase { adv, adj, noun })
     }
 }
 
@@ -67,30 +86,50 @@ impl Display for Phrase {
 }
 
 fn main() {
-    let matches = App::new("Git Release Names")
-        .author("Kevin Choubacha <chewbacha@gmail.com>")
-        .about("Takes a git sha and uses it's relatively unique combination of letters and number to generate a release name")
-        .arg(Arg::with_name("SHA")
-             .multiple(true)
-             .help("Each arg should be a sha. If they are less than 8 characters they will be padded"))
-        .get_matches();
-    let args = matches.values_of("SHA");
-    if let Some(shas) = args {
-        shas.for_each(|sha| println!("{}", Phrase::new(&sha)));
+    let matches = app_matches();
+
+    if let Some(shas) = matches.values_of("SHA") {
+        shas.for_each(|sha| println!("{}", &sha.parse::<Phrase>().unwrap()));
     } else if atty::is(Stream::Stdin) {
-        println!("{}", Phrase::new(&format!("{:8x}", rand::random::<usize>())));
+        from_random_sha()
     } else {
         // no args, check stdin
-        let stdin = io::stdin();
-        let mut reader = stdin.lock();
-        loop {
-            let mut line = String::new();
-            match reader.read_line(&mut line) {
-                Ok(size) if size > 0 => println!("{}", Phrase::new(&line.trim())),
-                _ => break,
-            }
-        }
+        from_stdin();
     };
+}
+
+fn app_matches() -> ArgMatches<'static> {
+    App::new("Git Release Names")
+        .author("Kevin Choubacha <chewbacha@gmail.com>")
+        .about(
+            "Takes a git sha and uses it's relatively unique combination of letters and number \
+                to generate a release name",
+        )
+        .arg(Arg::with_name("SHA").multiple(true).help(
+            "Each arg should be a sha. If they are less than 8 characters they will be padded",
+        ))
+        .get_matches()
+}
+
+fn from_random_sha() {
+    println!(
+        "{}",
+        &format!("{:8x}", rand::random::<usize>())
+            .parse::<Phrase>()
+            .unwrap()
+    );
+}
+
+fn from_stdin() {
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+    loop {
+        let mut line = String::new();
+        match reader.read_line(&mut line) {
+            Ok(size) if size > 0 => println!("{}", &line.trim().parse::<Phrase>().unwrap()),
+            _ => break,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -99,27 +138,33 @@ mod tests {
 
     #[test]
     fn it_can_parse_into_a_word() {
-        let word = Word::new("a");
+        let word = Word::from_str("a").unwrap();
         assert_eq!(word.hash, 10);
     }
 
     #[test]
+    fn it_can_detect_non_hex_chars() {
+        let result = Word::from_str("z");
+        assert!(result.is_err());
+    }
+
+    #[test]
     fn it_can_look_up_from_a_dictionary() {
-        let word = Word::new("a").lookup(&adverbs::WORDS);
+        let word = lookup(Word::from_str("a").unwrap(), &adverbs::WORDS);
         assert_eq!(word.word, Some("proximally".to_string()));
     }
 
     #[test]
     fn it_can_format_the_word() {
-        let word = Word::new("a");
+        let word = Word::from_str("a").unwrap();
         assert_eq!("", format!("{}", word));
-        let word = word.lookup(&adverbs::WORDS);
+        let word = lookup(word, &adverbs::WORDS);
         assert_eq!("proximally", format!("{}", word));
     }
 
     #[test]
-    fn a_phrase_can_be_generated() {
-        let phrase = Phrase::new("0a00a00a");
+    fn a_phrase_can_be_generated_from_a_str() {
+        let phrase = "0a00a00a".parse::<Phrase>().expect("Invalid phrase");
         assert_eq!("immeasurably endways borings", format!("{}", phrase));
     }
 }
